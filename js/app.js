0a3966135556da45df053e3864d3b54cc638b499
hryktkuch <hightraffic@gmail.com>
2024-05-06 15:00:15 +0900

    いったん完成

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/*const judge_criteria = [
+const judge_criteria = [
     [0, 1, -1],
     [-1, 0, 1],
     [1, -1, 0]
@@ -9,73 +9,99 @@ function judge(human){
     result=judge_criteria[human][random];
     return result;
 }
-*/
 
-$("#paper").on("click", function() {
-    $("#rock").css("opacity", "0.2")
-    $("#scissor").css("opacity", "0.2")
-    $("#paper").css("opacity", "1")
+function human_hand(x) {
+    if (x == 0) {
+        $("#rock").css("opacity", "0.2")
+        $("#scissor").css("opacity", "0.2")
+        $("#paper").css("opacity", "1")
+    } else if (x == 1) {
+        $("#rock").css("opacity", "0.2")
+        $("#scissor").css("opacity", "1")
+        $("#paper").css("opacity", "0.2")
+    } else {
+        $("#rock").css("opacity", "1")
+        $("#scissor").css("opacity", "0.2")
+        $("#paper").css("opacity", "0.2")
+    }
+}
+
+function com_hand(x) {
     var random=Math.floor(Math.random()*3);
     if (random == 0) {
-        $("#com_rock").css("opacity","1")
+        $("#com_rock").css("opacity", "0.2")
         $("#com_scissor").css("opacity", "0.2")
-        $("#com_paper").css("opacity", "0.2")
-        $("#result").text("あなたの勝ち！")
+        $("#com_paper").css("opacity", "1")
     } else if (random == 1) {
         $("#com_rock").css("opacity", "0.2")
         $("#com_scissor").css("opacity", "1")
         $("#com_paper").css("opacity", "0.2")
-        $("#result").text("あなたの負け！")
     } else {
-        $("#com_rock").css("opacity", "0.2")
+        $("#com_rock").css("opacity", "1")
         $("#com_scissor").css("opacity", "0.2")
-        $("#com_paper").css("opacity", "1")
-        $("#result").text("あいこ！")
+        $("#com_paper").css("opacity", "0.2")
+    }
+
+    if (judge_criteria[x][random] == 0) {
+        $("#draw").css("opacity", "1")
+        $("#win").css("opacity", "0.2")
+        $("#lose").css("opacity", "0.2")
+    } else if (judge_criteria[x][random] == 1) {
+        $("#draw").css("opacity", "0.2")
+        $("#win").css("opacity", "0.2")
+        $("#lose").css("opacity", "1")
+    } else {
+        $("#draw").css("opacity", "0.2")
+        $("#win").css("opacity", "1")
+        $("#lose").css("opacity", "0.2")
+    }
+}
+
+$("#paper").on("click", function() {
+    human = 0;
+    human_hand(human);
+    com_hand(human);
+})
+$("#paper").on("mouseover", function() {
+    $("#paper").css("opacity", "0.6")
+})
+$("#paper").on("mouseout", function() {
+    if (human==0) {
+        $("#paper").css("opacity", "1")
+    } else {
+        $("#paper").css("opacity", "0.2")
     }
 })
 
 $("#scissor").on("click", function() {
-    $("#rock").css("opacity", "0.2")
-    $("#scissor").css("opacity", "1")
-    $("#paper").css("opacity", "0.2")
-    var random=Math.floor(Math.random()*3);
-    if (random == 0) {
-        $("#com_rock").css("opacity","1")
-        $("#com_scissor").css("opacity", "0.2")
-        $("#com_paper").css("opacity", "0.2")
-        $("#result").text("あなたの負け！")
-    } else if (random == 1) {
-        $("#com_rock").css("opacity", "0.2")
-        $("#com_scissor").css("opacity", "1")
-        $("#com_paper").css("opacity", "0.2")
-        $("#result").text("あいこ！")
+    human = 1;
+    human_hand(human);
+    com_hand(human);
+})
+$("#scissor").on("mouseover", function() {
+    $("#scissor").css("opacity", "0.6")
+})
+$("#scissor").on("mouseout", function() {
+    if (human==1) {
+        $("#scissor").css("opacity", "1")
     } else {
-        $("#com_rock").css("opacity", "0.2")
-        $("#com_scissor").css("opacity", "0.2")
-        $("#com_paper").css("opacity", "1")
-        $("#result").text("あなたの勝ち！")
+        $("#scissor").css("opacity", "0.2")
     }
 })
 
 $("#rock").on("click", function() {
-    $("#rock").css("opacity", "1")
-    $("#scissor").css("opacity", "0.2")
-    $("#paper").css("opacity", "0.2")
+    human = 2;
+    human_hand(human);
     var random=Math.floor(Math.random()*3);
-    if (random == 0) {
-        $("#com_rock").css("opacity","1")
-        $("#com_scissor").css("opacity", "0.2")
-        $("#com_paper").css("opacity", "0.2")
-        $("#result").text("あいこ！")
-    } else if (random == 1) {
-        $("#com_rock").css("opacity", "0.2")
-        $("#com_scissor").css("opacity", "1")
-        $("#com_paper").css("opacity", "0.2")
-        $("#result").text("あなたの勝ち！")
+    com_hand(human);
+})
+$("#rock").on("mouseover", function() {
+    $("#rock").css("opacity", "0.6")
+})
+$("#rock").on("mouseout", function() {
+    if (human==2) {
+        $("#rock").css("opacity", "1")
     } else {
-        $("#com_rock").css("opacity", "0.2")
-        $("#com_scissor").css("opacity", "0.2")
-        $("#com_paper").css("opacity", "1")
-        $("#result").text("あなたの負け！")
+        $("#rock").css("opacity", "0.2")
     }
 })
